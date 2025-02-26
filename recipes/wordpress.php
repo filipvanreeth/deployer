c56c4c10bc3069194c1a3be0c0b3d9938a6ebc8d
Filip Van Reeth <filip@filipvanreeth.com>
2025-02-26 14:12:49 +0100

    feat: add tasks for installing languages, updating WordPress core, and checking plugin updates

--- recipes/wordpress.php
+++ recipes/wordpress.php
@@ -16,3 +16,85 @@ desc('Set administration email address');
 task('wordpress:set_admin_email', function () {
     runWpQuery('wordpress/admin-email');
 });
+
+desc('Installs languages for plugins and themes (locally)');
+task('wordpress:install_languages', function () {
+    $wp = getWpCommand();
+
+    // Get the list of installed languages
+    $languages = runLocally("{$wp} core language list --status=installed --field=language");
+
+    if (empty(trim($languages))) {
+        writeln('<info>No languages installed.</info>');
+        return;
+    }
+
+    $languages = explode("\n", trim($languages));
+
+    foreach ($languages as $language) {
+        writeln("<info>Installing plugins and themes for language: $language</info>");
+
+        // Install plugins for the specified language
+        $pluginsOutput = runLocally("{$wp} language plugin install --all --color $language", ['tty' => true]);
+        writeln($pluginsOutput);
+
+        // Install themes for the specified language
+        $themesOutput = runLocally("{$wp} language theme install --all --color $language", ['tty' => true]);
+        writeln($themesOutput);
+    }
+
+    writeln('<info>Installation of plugins and themes for all languages completed.</info>');
+});
+
+desc('Updates WordPress core, plugins, and themes');
+task('wordpress:update', function () {
+    $wp = getWpCommand();
+
+    // Update WordPress core
+    $coreOutput = runLocally("{$wp} core update --color", ['tty' => true]);
+    writeln($coreOutput);
+
+    // Update plugins
+    $pluginsOutput = runLocally("{$wp} plugin update --all --color", ['tty' => true]);
+    writeln($pluginsOutput);
+
+    // Update themes
+    $themesOutput = runLocally("{$wp} theme update --all --color", ['tty' => true]);
+    writeln($themesOutput);
+});
+
+desc('Checks for plugin updates');
+task('wordpress:check_plugin_updates', function () {
+    $wp = 'wp';
+    
+    if(currentHost()->has('lando')) {
+        $wp = 'lando wp';
+    }
+    
+    if(currentHost()->getAlias() === 'localhost') {
+        var_dump('running locally');
+        $plugins = runLocally("{$wp} plugin list --update=available --format=json --path={{web_root}}/wp");
+    } else {
+        $wpPath = "{{current_path}}/{{web_root}}/wp";
+        $plugins = run("{$wp} plugin list --update=available --format=json --path={$wpPath}");
+    }
+
+    if (empty(trim($plugins))) {
+        writeln('<info>All plugins are up to date.</info>');
+        return;
+    }
+
+    $plugins = json_decode($plugins, true);
+    $totalPluginUpdates = count($plugins);
+
+    $output = "<info>{$totalPluginUpdates} plugins with available updates:</info>" . PHP_EOL;
+
+    foreach ($plugins as $plugin) {
+        $pluginName = $plugin['name'];
+        $currentVersion = $plugin['version'];
+        $newVersion = $plugin['update_version'];
+        $output .= "{$pluginName} ({$currentVersion} -> {$newVersion})" . PHP_EOL;
+    }
+
+    writeln($output);
+});
