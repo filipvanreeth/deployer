dbedf5237efafac701629cf168c41d21843af443
Filip Van Reeth <filip@filipvanreeth.com>
2025-02-26 16:57:15 +0100

    feat: add task to cleanup default files and directories

--- recipes/cleanup.php
+++ recipes/cleanup.php
@@ -8,17 +8,33 @@ task('cleanup:unused_themes', function () {
     run("rm -rf {$webRoot}/wp/wp-content/themes/twenty*");
 });
 
+desc('Cleanup default files and directories');
+task('cleanup:default_files', function () {
+    $filesToDelete = [];
+
+    if (empty($filesToDelete)) {
+        return;
+    }
+
+    $releasePath = get('release_path');
+    $webRoot = get('web_root');
+
+    foreach ($filesToDelete as $file) {
+        run("rm -f {$releasePath}/{$webRoot}/{$file}");
+    }
+});
+
 desc('Cleanup WordPress files');
 task('cleanup:wordpress_files', function () {
     $releasePath = get('release_path');
     $webRoot = get('web_root');
-    
+
     $filesToDelete = [
         'wp-config-sample.php',
         'license.txt',
         'readme.html',
     ];
-    
+
     foreach ($filesToDelete as $file) {
         run("rm -f {$releasePath}/{$webRoot}/wp/{$file}");
     }
