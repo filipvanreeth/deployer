2f19d5b97e7643cc022e86c97a54c87a580fb8ca
Filip Van Reeth <filip@filipvanreeth.com>
2025-02-26 16:57:33 +0100

    fix: correct description for unused themes cleanup task

--- recipes/cleanup.php
+++ recipes/cleanup.php
@@ -2,7 +2,7 @@
 namespace Deployer;
 
 /** Clean up */
-desc('Clean up unused themes');
+desc('Cleanup unused themes');
 task('cleanup:unused_themes', function () {
     $webRoot = get('web_root');
     run("rm -rf {$webRoot}/wp/wp-content/themes/twenty*");
