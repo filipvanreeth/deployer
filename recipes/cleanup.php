f32bea14f0cf9c4427d7e48d6ffd402284c84b66
Filip Van Reeth <filip@filipvanreeth.com>
2025-02-26 16:53:35 +0100

    feat: add task to cleanup unnecessary WordPress files

--- recipes/cleanup.php
+++ recipes/cleanup.php
@@ -7,3 +7,19 @@ task('cleanup:unused_themes', function () {
     $webRoot = get('web_root');
     run("rm -rf {$webRoot}/wp/wp-content/themes/twenty*");
 });
+
+desc('Cleanup WordPress files');
+task('cleanup:wordpress_files', function () {
+    $releasePath = get('release_path');
+    $webRoot = get('web_root');
+    
+    $filesToDelete = [
+        'wp-config-sample.php',
+        'license.txt',
+        'readme.html',
+    ];
+    
+    foreach ($filesToDelete as $file) {
+        run("rm -f {$releasePath}/{$webRoot}/wp/{$file}");
+    }
+});
