6ed0a46c59456ea4619df19ac7116e25d7a5f9a1
Filip Van Reeth <filip@filipvanreeth.com>
2025-02-27 10:41:52 +0100

    fix: remove debug output from getWpCommand function

--- functions.php
+++ functions.php
@@ -73,6 +73,5 @@ function createSlug(string $string): string
 function getWpCommand()
 {
     $isLandoRunning = run('lando info > /dev/null 2>&1 && echo "yes" || echo "no"');
-    var_dump($isLandoRunning);
     return ($isLandoRunning === 'yes') ? 'lando wp' : 'wp';
 }
